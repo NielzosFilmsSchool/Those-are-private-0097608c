49df9741cafe72de03af373cad065dc50b843402
NielzosFilmsSchool <niels.hazelaar@student.rocva.nl>
2020-03-02 11:46:47 +0100

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -1,35 +1,46 @@
 <?php
-class MyLogger {
-    public function log($message, $level){
+class MyLogger
+{
+    public function log($message, $level) 
+    {
         switch($level){
-            case "WARNING":
-                $this->warning($message);
-                break;
-            case "ERROR":
-                $this->error($message);
-                break;
-            case "INFO":
-                $this->info($message);
-                break;
-            case "DEBUG":
-                $this->warning($message);
-                break;
+        case "WARNING":
+            $this->warning($message);
+            break;
+        case "ERROR":
+            $this->error($message);
+            break;
+        case "INFO":
+            $this->info($message);
+            break;
+        case "DEBUG":
+            $this->warning($message);
+            break;
         }
     }
 
-    public function warning($message){
-        $this->logWithTime("WARNING: ", $message);
+    public function warning($message)
+    {
+        $this->_logWithTime("WARNING: ", $message);
     }
-    public function error($message){
-        $this->logWithTime("ERROR: ", $message);
+
+    public function error($message)
+    {
+        $this->_logWithTime("ERROR: ", $message);
     }
-    public function info($message){
-        $this->logWithTime("INFO: ", $message);
+    
+    public function info($message)
+    {
+        $this->_logWithTime("INFO: ", $message);
     }
-    public function debug($message){
-        $this->logWithTime("DEBUG: ", $message);
+
+    public function debug($message)
+    {
+        $this->_logWithTime("DEBUG: ", $message);
     }
-    private function logWithTime($type, $message){
+
+    private function _logWithTime($type, $message)
+    {
         echo "[".date("d,M,Y h:i:s")."] $type $message".PHP_EOL;
     }
 }
